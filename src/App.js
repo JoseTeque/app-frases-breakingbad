65c5b898ea5bafff188ecb0e71dfc33a342c9a47
Jose Guerra <jrguerra.180490@gmail.com>
2020-07-05 03:02:38 -0400

    app-frases-brreakingbad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState , useEffect} from 'react';
+import styled from '@emotion/styled';
+import Frase from './Frase'
+
+
+
+const Contenedor = styled.div`
+  display:flex;
+  align-items:center;
+  padding-top:4rem;
+  flex-direction:column;
+`;
+
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top:3rem;
+  padding:1rem 3rem;
+  font-size:2rem;
+  border:2px solid black;
+
+  &:hover{
+    cursor: pointer;
+  }
+`;
 
 function App() {
+
+  const [frase, setfrase] = useState({})
+
+  const consultarApi = async() => {
+    const respuesta = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const resultado = await respuesta.json();
+    const frase = resultado[0];
+    setfrase(frase);
+  }
+
+  useEffect(() => {
+   
+    consultarApi();
+   
+  }, [])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Contenedor>
+
+        <Frase
+          frase = {frase}
+        />
+        <Boton
+          onClick={consultarApi}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            Obtener Frase
+        </Boton>
+    </Contenedor>
+      
   );
 }
 
---
+++ src/Frase.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorFrase = styled.div`
+    padding:3rem;
+    border-radius:.5rem;
+    background-color:#fff;
+    max-width:800px;
+    margin-top:20rem;
+
+    h1{
+        font-family:Arial, Helvetica, sans-serif;
+        text-align:center;
+        position:relative;
+        padding-left:4rem;
+
+        &::before{
+            content:open-quote;
+            font-size:8rem;
+            color:black;
+            position:absolute;
+            left:-1rem;
+            top:-3rem;
+        }
+    }
+
+    p{
+        font-family:Verdana, Geneva, Tahoma, sans-serif;
+        font-size:1.6rem;
+        text-align:right;
+        color:#666;
+        margin-top:2rem;
+        font-weight:bold;
+    }
+`;
+
+const Frase = ({frase}) => {
+
+ 
+    const {quote , author} = frase;
+
+    return ( 
+        <ContenedorFrase>
+            <h1>{quote}</h1>
+            <p>-{author}</p>
+        </ContenedorFrase>
+
+     );
+}
+ 
+export default Frase;
